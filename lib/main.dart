611d2571f9f3b06d13d770bd01d8cb0521e698ad
Ahmed-0331 <ahmedjoha650@gmail.com>
2024-09-26 21:09:20 +0600

    new app

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class Book {
   }
   void main() {
     Book book1 = Book("The Alchemist","A", 2019,50);
-    Book book2 = Book("The George Orwell","P" 2020,100);
+    Book book2 = Book("The George Orwell","P",2020,100);
     Book book3 = Book("The Great Gatsby","F", 2024,50);
     book1.read(20);
     book2.read(50);
